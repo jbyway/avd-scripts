6a90112fc09b6c019a996aad5c3c65ad8df6d19d
Jason Byway <jason.byway@microsoft.com>
2023-09-18 20:49:41 +1000

    sdaf

--- win10towin11IPU/prepareupgrade.ps1
+++ win10towin11IPU/prepareupgrade.ps1
@@ -239,4 +239,4 @@ function set-windowsmediacleanuptask
 # Remove-Item -Path $tempFolder -Force -Recurse
 
 # Run the script
-start-windowssetup -downloadUrl [uri]$downloadUrl -quiet $quiet -SkipFinalize $SkipFinalize -Finalize $Finalize -ScanOnly $ScanOnly -dynamicUpdate $dynamicUpdate
+start-windowssetup -downloadUrl $downloadUrl -quiet $quiet -SkipFinalize $SkipFinalize -Finalize $Finalize -ScanOnly $ScanOnly -dynamicUpdate $dynamicUpdate
